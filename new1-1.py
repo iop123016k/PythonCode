87c15d8ecf7dcecce2ed93740e879ddc4c4710ee
iop123016k@yahoo.com.tw <sarah011>
2014-09-29 15:17:48 +0800

    Revert "ok"
    This reverts commit a9ec7d6ce16c176a985ef9244016065a1d1e2680.
    
    Conflicts:
    	Chapter1.py
    	Chapter1_4.py

---
+++ new1-1.py
@@ -0,0 +1,4 @@
+print(type(3))
+print(int(3.14159),int(-2.8))
+print(float(3),float(-1))
+print(1+3*2**5)
---
+++ new1.py
@@ -0,0 +1,3 @@
+#print (我的第一隻python程式")
+name=input('what is your name?\n')
+print('Hi,%s.'%name)
